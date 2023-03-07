c245ea27fb2f79a4f46ca3129463ba6a61c3273e
Paweł Siemieniuk <89398353+pawelsiemieniuk@users.noreply.github.com>
2023-03-07 17:07:41 +0100

    README update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# alarm-clock-MSP430-EasyWeb2
- Clock with 3 slots of custom alarms, created for MSP430-EasyWeb2 board.
+# Alarm Clock for MSP430-EasyWeb2
+
+ Program was compiled and sent to board using IAR Embedded Workbench. Required libraries for LCD are included in [used_libraries](/used_libraries). Documentation and diagram of MSP430-EasyWeb2 used for creating this project can be found in [docs](/docs).
+
+ Functionalities:
+ * Displaying time on LCD in format 00:00:00:00 (On start, time is set to value defined in main.c file).
+ * Saving up to 3 alarms and turning on buzzer few times at specified hour.
+ 
+ Usage:
+ * Click 4th button (first on the right) to turn on alarm setting mode. Now display shows time for first alarm slot.
+ * Clicking buttons 1-3 changes, starting from left: hour, minute, second, of edited alarm.
+ * To save alarm click again 4th button.
+ * Repeating process creates new alarms up to 3, 4th alarm will overwrite 1st.
+
+ Project was made with Szymon Siłkin on Computer Organization and Architecture class at Bialystok University of Technology.
--- main.c
+++ main.c
@@ -22,14 +22,21 @@ void displayTime();
 void displaySavedTime();
 void setAlarm();
 
-unsigned long INTERRUPT_IN_SEC = sec_us / INTERRUPT_TIME_us;// Liczba przerwan w ciagu sekundy
 unsigned long B1_TIME = 0, B1_STATUS = 0;                   // Licznik przerwan po wcisnieciu
 unsigned long B2_TIME = 0, B2_STATUS = 0;                   // przyciskow B1, B2, B3, B4
 unsigned long B3_TIME = 0, B3_STATUS = 0;                   // oraz ich status
 unsigned long B4_TIME = 0, B4_STATUS = 0;                   //
+
+unsigned long INTERRUPT_IN_SEC = sec_us / INTERRUPT_TIME_us;// Liczba przerwan w ciagu sekundy
 unsigned long COUNTER = CURR_T;                             // Zmienna zliczajaca przerwania
+
 unsigned int SET_ALARM = 0;                                 // Czy alarm jest ustawiany
 unsigned int BUZZER_TIME = 0;                               // Czas do konca dzialania alarmu
+
+unsigned int hours = 0;
+unsigned int minutes = 0;
+unsigned int seconds = 0;
+unsigned int miliseconds = 0;
 
 unsigned int SAVED_TIME[3][4] = {{0,0,0,0},                 // Tablica zapisanych alarmow
                                  {0,0,0,0},
